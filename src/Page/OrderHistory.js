3118d8a441bffa1edfeff989ab3419d9e487a9c2
Shriya Lambat <shriyalambat@gmail.com>
2022-04-06 01:16:47 +0530

    Create OrderHistory.js

---
+++ src/Page/OrderHistory.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../Components/Nav";
+import Cards from "../Components/Cards"
+import Footer from "../Components/Footer"
+const OrderHistory = () => {
+  return (
+    <div>
+      <Nav />
+      <Cards></Cards>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default OrderHistory;
