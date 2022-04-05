9bd51a43c3a81a21aac27f847db41954063f3415
Shriya Lambat <shriyalambat@gmail.com>
2022-04-06 01:17:36 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import OrderHistory from "./Page/OrderHistory";
+const App = () => {
+  return <OrderHistory />;
+};
+export default App;
